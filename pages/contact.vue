0d88659ba5f09b7628d0a6dc00d1a374363024b0
Bianca <bsineleck@gmail.com>
2025-07-03 20:00:55 -0300

    contact page started

---
+++ pages/contact.vue
@@ -0,0 +1,126 @@
+<template>
+  <section id="contact" class="container">
+    <div class="fade-in-on-scroll" style="--animation-delay: 0.1s;">
+        <h3>Contato</h3>
+    </div>
+    <div class="contact-grid">
+        <div class="contact-links card fade-in-on-scroll" style="--animation-delay: 0.2s;">
+            <h5>Entre em contato</h5>
+            <p>Ficarei feliz em responder sua mensagem.</p>
+            <div class="social-links">
+                <a href="https://www.linkedin.com/in/biancasineleck/" target="_blank" class="linkedin">
+                    <div class="social-img"><svg stroke="currentColor" fill="#B5E0F3" stroke-width="0" viewBox="0 0 448 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"></path></svg></div>
+                    <p>Bianca Sineleck</p>
+                </a>
+                <a href="https://github.com/Beawnk" target="_blank" class="github">
+                    <div class="social-img"><svg stroke="currentColor" fill="#F8C8DC" stroke-width="0" viewBox="0 0 496 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg></div>
+                    <p>Beawnk</p>
+                </a>
+                <a href="mailto:bsineleck@hotmail.com" class="email">
+                    <div class="social-img"><svg stroke="#FCEEB5" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg></div>
+                    <p>bsineleck@hotmail.com</p>
+                </a>
+                <a href="https://wa.me/5551996404334" target="_blank">
+                    <div class="social-img"><svg fill="#D1F4E1" height="200px" width="200px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M17,0C8.7,0,2,6.7,2,15c0,3.4,1.1,6.6,3.2,9.2l-2.1,6.4c-0.1,0.4,0,0.8,0.3,1.1C3.5,31.9,3.8,32,4,32c0.1,0,0.3,0,0.4-0.1 l6.9-3.1C13.1,29.6,15,30,17,30c8.3,0,15-6.7,15-15S25.3,0,17,0z M25.7,20.5c-0.4,1.2-1.9,2.2-3.2,2.4C22.2,23,21.9,23,21.5,23 c-0.8,0-2-0.2-4.1-1.1c-2.4-1-4.8-3.1-6.7-5.8L10.7,16C10.1,15.1,9,13.4,9,11.6c0-2.2,1.1-3.3,1.5-3.8c0.5-0.5,1.2-0.8,2-0.8 c0.2,0,0.3,0,0.5,0c0.7,0,1.2,0.2,1.7,1.2l0.4,0.8c0.3,0.8,0.7,1.7,0.8,1.8c0.3,0.6,0.3,1.1,0,1.6c-0.1,0.3-0.3,0.5-0.5,0.7 c-0.1,0.2-0.2,0.3-0.3,0.3c-0.1,0.1-0.1,0.1-0.2,0.2c0.3,0.5,0.9,1.4,1.7,2.1c1.2,1.1,2.1,1.4,2.6,1.6l0,0c0.2-0.2,0.4-0.6,0.7-0.9 l0.1-0.2c0.5-0.7,1.3-0.9,2.1-0.6c0.4,0.2,2.6,1.2,2.6,1.2l0.2,0.1c0.3,0.2,0.7,0.3,0.9,0.7C26.2,18.5,25.9,19.8,25.7,20.5z"></path> </g></svg></div>
+                    <p>+55 (51) 996404334</p>
+                </a>
+            </div>
+        </div>
+        <div class="contact-form card fade-in-on-scroll" style="--animation-delay: 0.4s;">
+            <form action="https://formspree.io/f/xjvowzqk" method="POST">
+                <label for="name">Nome</label>
+                <input type="text" id="name" name="name" required placeholder="Seu nome">
+                
+                <label for="email">E-mail</label>
+                <input type="email" id="email" name="_replyto" required placeholder="Seu e-mail">
+                
+                <label for="message">Mensagem</label>
+                <textarea id="message" name="message" rows="4" required placeholder="Sua mensagem"></textarea>
+                
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+useAnimateOnScroll('.fade-in-on-scroll');
+</script>
+
+<style lang="scss" scoped>
+#contact {
+
+    h3 {
+        margin-bottom: 50px;
+        text-align: center;
+        color: var(--highlight-color-5);
+    }
+
+    .contact-grid {
+        display: grid;
+        grid-template-columns: repeat(6, 1fr);
+        grid-template-rows: repeat(6, 1fr);
+        gap: 25px;
+
+        .card {
+            background-color: var(--dark-color);
+            padding: 30px;
+            border-radius: var(--border-radius);
+        }
+
+        .contact-links {
+            padding: 100px;
+            grid-column: span 3 / span 3;
+            grid-row: span 6 / span 6;
+
+            h5 {
+                color: var(--text-color);
+                margin-bottom: 20px;
+            }
+
+            p {
+                color: var(--text-color);
+                font-size: var(--text-medium);
+            }
+
+            .social-links {
+                display: flex;
+                flex-direction: column;
+                gap: 15px;
+                margin-top: 30px;
+
+                a {
+                    display: flex;
+                    align-items: center;
+                    color: var(--text-color);
+                    transition: var(--transition);
+
+                    &:hover {
+                        text-decoration: underline;
+                    }
+
+                    .social-img {
+                        width: 24px;
+                        height: 24px;
+                        margin-right: 10px;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+
+                        svg, img {
+                            width: 100%;
+                            height: 100%;
+                        }
+                    }
+                }
+            }
+        }
+
+        .contact-form {
+            grid-column: span 3 / span 3;
+            grid-row: span 6 / span 6;
+        }
+    }
+}
+</style>
